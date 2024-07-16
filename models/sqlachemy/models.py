827b71ab90e137bc8629bf69b118c4302e6556dc
GTF08 <ggttff0088@gmail.com>
2024-07-16 07:01:12 +0300

    Added docker

--- models/sqlachemy/models.py
+++
@@ -1,9 +0,0 @@
-from extensions import Base
-from sqlalchemy import Column, Integer, String
-
-class Meme(Base):
-    __tablename__ = "memes"
-
-    id = Column(Integer, primary_key=True)
-    text = Column(String(64))
-    image_uuid = Column(String(128))
